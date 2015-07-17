975b3a656e2bbb35258c6a656aeebeda9265ad41
Rahul Doshi <rahuld223@yahoo.com>
2015-07-17 10:35:52 -0700

    feat(reporter): show relative path of each file with errors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var stylish = require('jshint-stylish');
+var stylish = require('jshint-stylish'),
+    path = require('path');
 
 module.exports = function(file) {
     return stylish.reporter(file.htmlhint.messages.map(function(errMsg) {
         return {
-            file: errMsg.file,
+            file: path.relative(file.cwd, errMsg.file),
             error: {
                 character: errMsg.error.col,
                 code: errMsg.error.rule.id,
